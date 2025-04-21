7ef7df373956785e636c4b32db4e5964a599bc71
Hirwa Rene <hirwarene6@gmail.com>
2025-04-21 19:45:39 +0200

    created list route

--- start/routes.ts
+++ start/routes.ts
@@ -12,8 +12,10 @@ import ShortUrlsController from '#controllers/short_urls_controller'
 import QrCodesController from '#controllers/qr_codes_controller'
 
 router.get('/', [ShortUrlsController, 'index'])
+router.get('/list', '#resources/views/pages/url_list.submit')
 router.post('/shorten', [ShortUrlsController, 'create'])
 router.get('/:code', [ShortUrlsController, 'redirect'])
 router.get('/qr/:code', [QrCodesController, 'generate'])
+
 
 
