7030f315f0ba5bf3cb5e9a176c9e120f87cf12cb
Hirwa Rene <hirwarene6@gmail.com>
2025-04-24 11:36:39 +0200

    created auth controller

--- app/controllers/user_controller.ts
+++ app/controllers/user_controller.ts
@@ -1,6 +1,12 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/user'
 
-export default class UserController {
+export default class UsersController {
+  public async userProfile({ request }: HttpContext) {
+    const data = request.all()
+    const payload = await createUserValidator.validate(data)
+    return payload
+  }
   
+
 }
