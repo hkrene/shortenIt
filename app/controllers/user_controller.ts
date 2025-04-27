96f9a0945a05ba9a3fe2e920a468d68aa66e0f13
Hirwa Rene <hirwarene6@gmail.com>
2025-04-27 21:57:27 +0200

    defined routes for links of header component

--- app/controllers/user_controller.ts
+++ app/controllers/user_controller.ts
@@ -5,7 +5,7 @@ import User from '#models/user'
 
 export default class UserController {
 
-  public async store({ view, request}:HttpContext) {
+  public async store({ view, request, response}:HttpContext) {
       const payload = await request.validateUsing(createUserValidator)
       
       const user = await User.create({
@@ -15,14 +15,16 @@ export default class UserController {
       })
       console.log(user);
 
-      return view.render('pages/url_list')
+      return response.redirect('/list')
     }
 
 
   public async login({ request, response, auth, session }: HttpContext) {
     try {
       const { email, password } = request.only(['email', 'password'])
+
       const user = await User.verifyCredentials(email, password)
+      
       await auth.use('web').login(user)
       session.flash('success', 'Login successful')
       return response.redirect('/list')
